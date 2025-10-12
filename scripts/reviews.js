4baa527ce19bac3dbf9495ac299d60cbfbfbfbf0
Domische <domische.07@mail.ru>
2025-10-12 21:17:07 +0300

    Fix a bug in the reviews section

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -3,34 +3,70 @@ const rootSelector = '[data-js-reviews]';
 function reviews(rootElement) {
     const selectors = {
         reviewsAvatar: '[data-js-reviews-avatar]',
-        reviewsInfo: '[data-js-reviews-info]',
+        reviewsItem: '[data-js-reviews-item]',
+        reviewsBody: '[data-js-reviews-body]',
     }
 
     const stateClasses = {
-        isActive: 'is-active'
+        isActive: 'is-active',
+        isOnTheLeftSide: 'is-on-the-left-side',
+        isOnTheBottomSide: 'is-on-the-bottom-side',
     }
 
-    const reviewsInfoElements = rootElement.querySelectorAll(selectors.reviewsInfo);
+    const initialState = {
+        activeIndex: 0
+    }
 
-    const onReviewsInfoClickOutside = () => {
-        reviewsInfoElements.forEach(reviewsInfoElement => {
-            reviewsInfoElement.classList.remove(stateClasses.isActive);
+    const reviewsItemElements = rootElement.querySelectorAll(selectors.reviewsItem);
+    const reviewsItemElementsArray = [...reviewsItemElements];
+
+    const onReviewsItemClickOutside = () => {
+        reviewsItemElementsArray.forEach(element => {
+            element.classList.remove(stateClasses.isActive);
         })
     }
 
-    const onReviewsAvatarClick = (reviewsInfoElement) => {
-        onReviewsInfoClickOutside();
-
-        reviewsInfoElement.classList.toggle(stateClasses.isActive);
+    const onReviewsAvatarClick = (reviewsItemElement) => {
+        reviewsItemElement.classList.toggle(stateClasses.isActive);
     }
 
-    document.addEventListener('click', (event) => {
-        const reviewsAvatarElement = event.target.closest(selectors.reviewsAvatar);
-        const reviewsInfoElement = event.target.closest(selectors.reviewsInfo);
+    const onReviewsClick = (event) => {
+        fixReviewsItemPosition();
+        const { target } = event;
+        const reviewsAvatarElement = target.closest(selectors.reviewsAvatar);
+        const reviewsItemElement = target.closest(selectors.reviewsItem);
+        const reviewsItemIndex = reviewsItemElementsArray.indexOf(reviewsItemElement);
 
-        reviewsAvatarElement && onReviewsAvatarClick(reviewsInfoElement);
-        !reviewsInfoElement && onReviewsInfoClickOutside();
-    })
+        if (reviewsItemIndex !== initialState.activeIndex) {
+            initialState.activeIndex = reviewsItemIndex;
+            onReviewsItemClickOutside();
+        }
+
+        reviewsAvatarElement && onReviewsAvatarClick(reviewsItemElement);
+    }
+
+    const fixReviewsItemPosition = () => {
+        const windowPositionX = document.documentElement.clientWidth; //clientWidth, а не window.innerWidth, потому что не нужно учитывать scrollbar
+
+        reviewsItemElementsArray.forEach((reviewsItemElement) => {
+            const reviewsBodyElement = reviewsItemElement.querySelector(selectors.reviewsBody);
+
+            const { left, right } = reviewsItemElement.getBoundingClientRect();
+
+            const reviewsItemElementColumnGap = +getComputedStyle(reviewsItemElement).getPropertyValue('--column-gap').replace('px', '');
+            const reviewsBodyElementWidth = +getComputedStyle(reviewsItemElement).getPropertyValue('--body-width').replace('px', '');
+
+            const isOnTheLeftSide = right + reviewsBodyElementWidth + reviewsItemElementColumnGap > windowPositionX;
+            const isOnTheBottomSide = left - reviewsBodyElementWidth - reviewsItemElementColumnGap < 0 && isOnTheLeftSide;
+
+            reviewsBodyElement.classList.toggle(stateClasses.isOnTheLeftSide, isOnTheLeftSide);
+            reviewsBodyElement.classList.toggle(stateClasses.isOnTheBottomSide, isOnTheBottomSide);
+        })
+    }
+
+    document.addEventListener('click', (event) => onReviewsClick(event))
+
+    window.addEventListener('resize', fixReviewsItemPosition)
 }
 
 function reviewsCollection() {
